3d6e2f24cffb0c0b1a142c603cc936361c3e3256
voidCaffelatte <>
2021-06-02 00:21:57 +0900

    Update "command line program" section

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -43,32 +43,16 @@ Trust me.";
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>
 {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines()
-    {
-        if line.to_lowercase().contains(&query)
-        {
-            results.push(line)
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> 
 {
-    let mut results = Vec::new();
-
-    for line in contents.lines()
-    {
-        if line.contains(query)
-        {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn run(config: Config) -> Result<(), Box<Error>>
@@ -104,16 +88,24 @@ pub struct Config
 
 impl Config
 {
-    pub fn new(args: &[String]) -> Result<Config, &'static str>
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str>
     {
-        if args.len() < 3
-        {
-            return Err("not enough arguments")
-        }
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next()
+        {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next()
+        {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
         Ok(Config { query, filename, case_sensitive })
     }
 }
